4a44ee5188a437552e7906a649abb2144f313c8c
Sam Mosleh <sam.mosleh@ut.ac.ir>
2021-06-23 14:40:25 +0430

    Update dependencies and remove poetry.lock file

--- scripts/format-imports.sh
+++ scripts/format-imports.sh
@@ -4,5 +4,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --recursive  --force-single-line-imports --apply .
+isort --force-single-line-imports .
 sh ./scripts/format.sh
